bffaef84e23058344410cfbd533a7e7505d34218
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-10-25 15:01:46 -0700

    conSpotter passed test 5
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

--- pig-latin-translator/js/scripts.js
+++ pig-latin-translator/js/scripts.js
@@ -1,6 +1,7 @@
-const vowelArray = ["a", "e", "i", "o", "u"];
+
 
 function vowelSpotter(word) {
+    const vowelArray = ["a", "e", "i", "o", "u"];
     if (vowelArray.includes(word.charAt(0))) {
         return word + "way";
     } else {
@@ -11,10 +12,10 @@ function vowelSpotter(word) {
 function conSpotter(word) {
     if (word.slice(0, 2) === "qu") {
         return word.slice(2) + "quay";
-
     }
     for (i = 0; i < word.length; i++) {
-        if (vowelArray.includes(word.charAt(i))) {
+        const vowelArrayAndQ = ["a", "e", "i", "o", "u", "q"];
+        if (vowelArrayAndQ.includes(word.charAt(i))) {
             const firstLetters = word.slice(0, i);
             const restLetters = word.slice(i);
             return restLetters + firstLetters + "ay";
